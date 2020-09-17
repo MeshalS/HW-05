e0a27bb5d9982518b2e89f6f2b5de4ac2ed9e99c
MeshalS <meshal.saus@gmail.com>
2020-09-17 08:49:07 +0300

    adding the fun HTML bulid

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ $(document).ready(function () {
     timeLine()
 
 });
+// this ffunction using tkaing the hour so it will set it and comper if it is PM or AM 
+function buildhtml(hour) {
+    let time = '';
+    if (hour > 11) {
+        time = hour + 'PM'
+    } else {
+        time = hour + 'AM'
+    }
+// to know the color 
+    let colorClass = calculateColor(hour);
+// creating the row and col and then have the tim where PMM or AM and tkaing the hour and onlcin 
+    let row = `<div class="row align-items-center" > 
+
+      <div class="col-1 border btn btn-lg" > ${time} </div>
+    
+      <div class="col-10 ">
+        <input id="${hour}"
+         class="w-20 border-0 outline-0 form-control form-control-lg ${colorClass}"  type="text" name="User-input" value='${getTask(hour)}' >
+      </div>
+    
+      <button class="col-1 bg-primary btn btn-lg text-white" onclick='saveTask("${hour}")' > Save </button>
+    </div>`
+    return row;
+}
